d91ba16e5da1de94daac491a8e576af9c09caae4
EC2 Default User <samuelbchapman@gmail.com>
2020-05-06 13:23:43 +0000

    updating translate.py to CL

--- translate.py
+++ translate.py
@@ -1,7 +1,18 @@
 import boto3
+import click
 
-client = boto3.client('translate', region_name="us-east-1")
-text = "hola mi nombre es Sam"
-result = client.translate_text(Text=text, SourceLanguageCode="auto",
-    TargetLanguageCode="en")
-print(result['TranslatedText'])
+@click.command()
+@click.option('--phrase', prompt='Put in a phrase in any language to translate',
+    help='this is a tool that translates text')
+
+def action(phrase):
+    client = boto3.client('translate', region_name="us-east-1")
+    click.echo(click.style(f"Translate phrase: {phrase}", fg='red'))
+    result = client.translate_text(Text=phrase, SourceLanguageCode="auto",
+        TargetLanguageCode="en")
+    text = result['TranslatedText']
+    click.echo(click.style(f'{text}', bg='blue', fg='white'))
+
+if __name__=='__main__':
+    action()
+    
